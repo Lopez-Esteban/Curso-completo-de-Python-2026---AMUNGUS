4d8e3a82a5e880df1853665a690f415d4474b818
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2026-06-27 21:36:17 -0300

    primer push

---
+++ Amungus_Vendedor.py
@@ -0,0 +1,12 @@
+# Amungus Ingresa Valor de Venta
+valor_amungus = float(input("Amungus ingresa valor de venta: "))
+
+# Calcular Impuesto Amungus (20%)
+amung_imp = valor_amungus * 0.20
+
+# Calcular Amungus Final
+precio_amungus_final = valor_amungus + amung_imp
+
+# Mostrar Amungu-Resultado
+print("El Impuesto Amungus es: ", amung_imp)
+print ("El Precio de Venta Amungus Final es: ", precio_amungus_final)
---
+++ amungus.py
@@ -0,0 +1,13 @@
+mensaje = input ("ingrese un texto: ")
+
+print (mensaje)
+
+n1 = input("Ingrese el primer numero: ")
+n2 = input("Ingrese el segundo numero: ")
+
+n1 = int(n1)
+n2 = int(n2)
+
+suma = n1 + n2
+
+print ("La suma es: ", suma)
